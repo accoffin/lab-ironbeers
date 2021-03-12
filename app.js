a1fcf12ded6ed674e09b3062e1c7831a3933621e
accoffin <alexandra_coffin@yahoo.com>
2021-03-11 17:17:44 -0700

    Fixed app.js commit.

--- app.js
+++ app.js
@@ -10,16 +10,23 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
+app.get('/', (req, res, next) => res.render('index'));
 
-// ...
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(error => console.log(error));
+});
 
-// Add the route handlers here:
-
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/random-beer', (req, res, next) => {
+  res.render('random-beer');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
